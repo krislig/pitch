ff965d3c1eea52990792746bfad38870ee192693
krislig <krislig@gmail.com>
2013-10-12 21:40:32 +0200

    added user and post models

--- models/db.py
+++ models/db.py
@@ -81,3 +81,7 @@ use_janrain(auth, filename='private/janrain.key')
 
 ## after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
+
+db.define_table('post',
+    Field('content', 'text'),
+    Field('user_id', db.auth_user))
